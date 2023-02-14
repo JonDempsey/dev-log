865718e7cc5c34de73ab5cc2d79603d84686768e
JonDempsey <jdempopspark@gmail.com>
2023-02-14 23:48:25 +0000

    added makeBody method

--- index.js
+++ index.js
@@ -11,6 +11,33 @@
           return distance;
         },
 
+        makeBody: function(type, {
+          velocityX = 0,
+          velocityY = 0,
+          rotationalVelocity = 0,
+          integrity = 1,
+          density = 1,
+          volatility = 0
+        } = {}) {
+          if (type === undefined) throw new Error('You must provide a valid String for the type parameter!');
+          return {
+            type: type,
+            velocityX: velocityX,
+            velocityY: velocityY,
+            rotationalVelocity: rotationalVelocity,
+            integrity: integrity,
+            density: density,
+            volatility: volatility,
+
+            handleCollision(impact, body) {
+              // template method //
+            },
+
+            update(event) {
+              // template method //
+            },
+          };
+        },
       },
     };
-  }(window, window._));
+  }(window, window._))
