2e5070647f01077a10533d5636cf5321fcd6741e
JonDempsey <jdempopspark@gmail.com>
2023-02-14 23:29:41 +0000

    added hypotenuse distance formula

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 (function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
+    window.myLibraryName = window.devlog || {
       numz: {},
-      phyz: {},
+      phyz: {
+
+        getDistance(pointA, pointB) {
+          const
+            distanceX = pointB.x - pointA.x,
+            distanceY = pointB.y - pointA.y,
+            distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+          return distance;
+        },
+
+      },
     };
   }(window, window._));
